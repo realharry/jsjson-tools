68b4dfd06ebcd658180f209dd4a158ce64b23911
Harry Y <sidewaybot@gmail.com>
2017-12-20 14:02:01 -0800

    Added '-' support. Published a new version (v 0.6).

--- jsoncat/README.md
+++ jsoncat/README.md
@@ -1,14 +1,16 @@
 # JsonCat
 > JSON file concatenator
 
-`jsoncat` concatenates and prints out JSON files to stdout.
+`jsoncat` concatenates multiple JSON files into a single array and prints out the output to the stdout.
+When a single JSON file is given, it does not add a top-level array.
+When no JSON file is given, it reads JSON from the stdin.
 
 
 ## Installation
 
 First, install [node.js](https://nodejs.org/) 
 (along with [npm](https://www.npmjs.com/))
-if you have already done so.
+if you haven't already done so.
 
 ```bash
 npm install -g @jsjson/jsoncat
@@ -20,12 +22,26 @@ The NPM package installs an executable `jsoncat` in your PATH.
 Run the script as follows:
 
 ```bash
-jsoncat -i=4 json-file1 json-file2 ...
+jsoncat -i4 json-file1 json-file2 ...
 ```
 
-The `-i` or `--indent` flag sets the indentation level when printing out the JSON files.
+The `-i` or `--indent` argument sets the indentation level when printing out the JSON files. 
 
-For usage info, try:
+Note that each json file should be a valid JSON.
+`jsoncat` concatenates the input json files, if more than one file is given, into a single top-level array.
+The merge flag, `-m` or `--merges`, merges the JSON files into the top-level array, if a given json file is a JSON array.
+
+When no json file is given, it reads from the stdin (until the EOF signal).
+The "-" symbol can also be used for stdin. For example, the following command
+
+```bash
+jsoncat -i4 json-file1 - json-file2
+```
+
+concatenates json-file1, stdin, and json-file2.
+
+
+For complete usage info, including examples, try:
 
 ```bash
 jsoncat -h
--- jsoncat/src/index.ts
+++ jsoncat/src/index.ts
@@ -1,56 +1,126 @@
+import * as readline from 'readline';
+// import fs = require('fs');
+import * as fs from 'fs';
+// import { readFile, readFileSync } from 'fs';
+// var os = require("os");
+import { EOL } from 'os';
 // import minimist = require('minimist');
 import * as minimist from 'minimist';
 // import { ParsedArgs } from 'minimist';
-// import fs = require('fs');
-import { readFile, readFileSync } from 'fs';
-// var os = require("os");
-import { EOL } from 'os';
 
 
 export class JsonCat {
+
+  private rl: readline.ReadLine;
 
   constructor(
     private files: string[],
     private indent: number = 2,
     private merge: boolean = false
   ) {
-    // ...
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      terminal: false
+    });
   }
 
+
+  // ????
+  // tbd: Need to use callback, or other async way...
+  private readFromStdin(cb = null): any {
+    let obj: (any | null) = null;
+    let content = '';
+    this.rl.on('line', function (line) {
+      // console.log('line: ' + line);
+      content += line;
+    }).on('close', function () {
+      // console.log('close: content = ' + content);
+      obj = JSON.parse(content);
+    })
+    return obj;
+  }
+
+  // TBD:
+  // Handle json parse errors???
   public doCat() {
     // if args is empty, read json from stdin.
     // otherwise "concat" the input files..
 
     let len = files.length;
     if (len == 0) {
-      console.log("Reading stdin is not supported yet.")
+      // console.log("Input your JSON files.")
+
+      // tbd: readFromStdin() currently does not work.
+      // let obj = this.readFromStdin();
+      // let json = JSON.stringify(obj, null, indent);
+      // // console.log('json = ' + json);
+      // process.stdout.write(json + EOL);
+
+      let content = '';
+      this.rl.on('line', function (line) {
+        // console.log('line: ' + line);
+        content += line;
+      }).on('close', function () {
+        // console.log('close: content = ' + content);
+        let obj = JSON.parse(content);
+        let json = JSON.stringify(obj, null, indent);
+        // console.log('json = ' + json);
+        process.stdout.write(json + EOL);
+      })
     } else if (len == 1) {
       let f = files[0];
-      readFile(f, 'utf8', function (err, content) {
-        // console.log('err = ' + err);
-        // console.log('content = ' + content);
-
-        if (!err) {
+      if (f == '-') {
+        let content = '';
+        this.rl.on('line', function (line) {
+          // console.log('line: ' + line);
+          content += line;
+        }).on('close', function () {
+          // console.log('close: content = ' + content);
           let obj = JSON.parse(content);
           let json = JSON.stringify(obj, null, indent);
           // console.log('json = ' + json);
           process.stdout.write(json + EOL);
-        } else {
-          console.log('err = ' + err);
-        }
-      });
+        })
+      } else {
+        fs.readFile(f, 'utf8', function (err, content) {
+          // console.log('err = ' + err);
+          // console.log('content = ' + content);
+          if (!err) {
+            let obj = JSON.parse(content);
+            let json = JSON.stringify(obj, null, indent);
+            // console.log('json = ' + json);
+            process.stdout.write(json + EOL);
+          } else {
+            console.log('err = ' + err);
+          }
+        });
+      }
     } else {
       // TBD: Use async version....
       // testing.
       let jsonArr: any[] = [];
       for (let f of files) {
-        let content = readFileSync(f, 'utf8');
-        // console.log('content = ' + content);
-
+        let content = '';
+        if (f == '-') {
+          // TBD:
+          // Does this work on Windows????
+          try {
+            content = fs.readFileSync('/dev/stdin', 'utf8');
+            // console.log('content = ' + content);            
+          } catch (ex) {
+            console.log("Failed to read stdin: ex = " + ex);
+          } finally {
+            process.stdin.end();
+          }
+        } else {
+          content = fs.readFileSync(f, 'utf8');
+          // console.log('content = ' + content);
+        }
         let obj = JSON.parse(content);
-        if(merge && obj.constructor === Array) {
+        if (merge && obj.constructor === Array) {
           let arr = obj as any[];
-          for(let a of arr) {
+          for (let a of arr) {
             jsonArr.push(a);
           }
         } else {
@@ -65,7 +135,7 @@ export class JsonCat {
 }
 
 // TBD: Keep this in sync with package.json.
-const version = '0.5';
+const version = '0.6';
 const versionInfo = `jsoncat ${version}
 Copyright (C) Harry Y. License MIT.
 This is free software: you are free to change and redistribute it.
@@ -80,6 +150,8 @@ SYNOPSIS
 DESCRIPTION
        Concatenate and pretty-print JSONFILE(s) to standard output.
 
+       With no JSONFILE, or when JSONFILE is -, read standard input.
+
        -i, --indent=INDENT
               set indent level to INDENT. The default value is 2.
 
@@ -93,14 +165,17 @@ DESCRIPTION
               output version information and exit
 
 EXAMPLES
-       jsoncat -i=4 f
-              Output f's contents using indentation level 4.
+       jsoncat
+              Reads stdin and outputs the contents to stdout as a JSON string.
 
-       jsoncat f1 f2
-              Concatenates f's contents into a new JSON array and outputs the JSON string to stdout.
+       jsoncat -i=4 f
+              Outputs f's contents using indentation level 4.
+
+       jsoncat f1 - f2
+              Concatenates the contents of f1, stdin, and f2 into a new JSON array and outputs the JSON string to stdout.
 
        jsoncat -m f1 f2
-              Merges f's contents into a single array and outputs the JSON string to stdout.
+              Merges the contents of f1 and f2 as a single array and outputs the JSON string to stdout.
 
 AUTHOR
        Written by Harry Y.
@@ -134,7 +209,7 @@ var versionRequested = false;
 if (argv.h) {
   helpRequested = true;
 } else {
-  if(argv.v) {
+  if (argv.v) {
     versionRequested = true;
   }
 }
